0baca4021bbd171bb42c43be36c69e82ab200d93
Olixpin <noibiabdulrasaq@gmail.com>
2022-10-11 04:52:34 +0100

    Header style built v1

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Header from "./Header";
 function App() {
   return (
     <div className="app">
-      <h1>Let's build a linked in clone</h1>
       {/* Header */}
       <Header />
+
       {/* App Body */}
       {/* Sidebar */}
       {/* Feed */}
